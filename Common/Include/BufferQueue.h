e175a31196b59322a182457bfd56bfc2936ce515
Nick Luo <nickluo@gmail.com>
2015-12-01 12:24:10 +0800

    Fix BufferQueue sync bug

--- Common/Include/BufferQueue.h
+++ Common/Include/BufferQueue.h
@@ -20,8 +20,9 @@ namespace TopGear
 		virtual ~BufferQueue();
 		BufferQueue(const BufferQueue&) = delete;            // disable copying
 		BufferQueue& operator=(const BufferQueue&) = delete; // disable assignment
-		bool Push(const T &item);
-		bool Pop(T &item, bool noWait = false);	//Keep waiting until any item popped, if noWait is true
+		bool Push(T item);
+		bool Pop(T &item);	//Keep waiting until any item popped, if noWait is true
+		bool Pop_NoWait(T &item);
 		void Discard();		//Stop popping waiting
 		bool Empty();
 	private:
@@ -36,7 +37,6 @@ namespace TopGear
 	BufferQueue<T>::BufferQueue(size_t limit)
 		:sizeLimit(limit), discarded(false)
 	{
-        //std::this_thread::get_id();
 	}
 
 	template <class T>
@@ -45,13 +45,13 @@ namespace TopGear
 	}
 
 	template <class T>
-	bool BufferQueue<T>::Push(const T &item)
+	bool BufferQueue<T>::Push(T item)
 	{
         std::lock_guard<std::mutex> lk(mtx);
 		auto result = false;
 		if (sizeLimit == 0 || queue.size() < sizeLimit)
 		{
-			queue.push(item);
+			queue.push(std::move(item));
 			cond.notify_one();
 			result = true;
 		}
@@ -59,24 +59,26 @@ namespace TopGear
 	}
 
 	template <class T>
-	inline bool BufferQueue<T>::Pop(T &item, bool noWait)
+	bool BufferQueue<T>::Pop_NoWait(T& item)
 	{
+		std::lock_guard<std::mutex> lk(mtx);
+		if (queue.empty())
+			return false;
+		item = std::move(queue.front());
+		queue.pop();
+		return true;
+	}
+
+	template <class T>
+	inline bool BufferQueue<T>::Pop(T &item)
+	{
+		std::unique_lock<std::mutex> lk(mtx);
 		discarded = false;
-        if (noWait)
-        {
-            std::lock_guard<std::mutex> lk(mtx);
-            if (queue.empty())
-                return false;
-        }
-        else
-        {
-            std::unique_lock<std::mutex> lk(mtx);
-            while (queue.empty())
-            {
-                cond.wait(lk);
-                if (discarded)
-                    return false;
-            }
+        while (queue.empty())
+		{
+			cond.wait(lk);
+			if (discarded)
+				return false;
         }
         item = std::move(queue.front());
 		queue.pop();
