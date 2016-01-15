72a30d011e87f5a8079498fdb985748b3635c263
Nick Luo <nickluo@gmail.com>
2016-01-15 16:01:29 +0800

    Update IDeviceControl

--- Common/Include/IDeviceControl.h
+++ Common/Include/IDeviceControl.h
@@ -3,11 +3,36 @@
 #include <cstdint>
 #include <string>
 #include <typeinfo>
+#include <memory>
+#include "IVideoStream.h"
 
 
 namespace TopGear
 {
-	class IPropertyData;
+	class IPropertyData
+	{
+	public:
+		virtual ~IPropertyData() = default;
+		virtual size_t GetTypeHash() const = 0;
+	};
+
+	template<class T>
+	class PropertyData final
+		: public IPropertyData
+	{
+	public:
+		PropertyData() {}
+		explicit PropertyData(const T &t) : Payload(t) {}
+		explicit PropertyData(T &&t) : Payload(t) {}
+
+		~PropertyData() override {}
+
+		size_t GetTypeHash() const override
+		{
+			return typeid(T).hash_code();
+		}
+		T Payload;
+	};
 
 	class IDeviceControl
 	{
@@ -18,42 +43,34 @@ namespace TopGear
 		virtual bool SetControl(std::string name, IPropertyData &&val) = 0;
 		virtual bool GetControl(std::string name, IPropertyData &val) = 0;
 
-		//virtual int SetSensorTrigger(uint8_t level) = 0;
-		//virtual int SetResyncNumber(uint16_t resyncNum) = 0;
-		//virtual int QueryDeviceRole() = 0;
-		//virtual std::string QueryDeviceInfo() = 0;
+		template<class T>
+		static bool GetProperty(std::shared_ptr<IVideoStream> &stream, std::string name, T &value);
+
+		template<class T>
+		static bool SetProperty(std::shared_ptr<IVideoStream> &stream, std::string name, T &&value);
 	};
 
-	class IPropertyData
+	template <class T>
+	bool IDeviceControl::GetProperty(std::shared_ptr<IVideoStream>& stream, std::string name, T &value)
 	{
-	public:
-		virtual ~IPropertyData() = default;
-		virtual size_t GetTypeHash() const = 0;
-		//virtual size_t GetTypeSize() const = 0;
-		//virtual void *Ptr() = 0;
-	};
-
-	template<class T>
-	class PropertyData final
-		: public IPropertyData
-	{
-	public:
-		PropertyData() {}
-		explicit PropertyData(T t) : Payload(t) {}
-		explicit PropertyData(T &t) : Payload(t) {}
-		virtual ~PropertyData() override {}
-		virtual size_t GetTypeHash() const override
+		auto deviceControl = std::dynamic_pointer_cast<IDeviceControl>(stream);
+		if (deviceControl == nullptr)
+			return false;
+		PropertyData<T> data;
+		if (deviceControl->GetControl(name, data))
 		{
-			return typeid(T).hash_code();
+			value = std::move(data.Payload);
+			return true;
 		}
-		//virtual size_t GetTypeSize() const override
-		//{
-		//	return sizeof(T);
-		//}
-		//virtual void *Ptr() override
-		//{
-		//	return &Payload;
-		//}
-		T Payload;
-	};
+		return false;
+	}
+
+	template <class T>
+	bool IDeviceControl::SetProperty(std::shared_ptr<IVideoStream>& stream, std::string name, T &&value)
+	{
+		auto deviceControl = std::dynamic_pointer_cast<IDeviceControl>(stream);
+		if (deviceControl == nullptr)
+			return false;
+		return deviceControl->SetControl(name, PropertyData<T>(value));
+	}
 }
